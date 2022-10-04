35c0089d02e77ec66bf216cc7a298f9f3f9fe45a
NASmirnovGit <nikus1727@gmail.com>
2022-10-04 20:54:32 +0700

    Initial commit

--- tests/first_test.py
+++ tests/first_test.py
@@ -12,20 +12,20 @@ class TestCalc:
         assert self.calc.multiply(self, 2, 2) ==5
 
     def test_division_calculate_corrently(self):
-        assert self.calc.division(self, 2, 2) == 4
+        assert self.calc.division(self, 6, 3) == 2
 
     def test_division_calculation_failed(self):
-        assert self.calc.division(self, 2, 2) == 4
+        assert self.calc.division(self, 10, 5) == 3
 
     def test_subtraction_calculate_corrently(self):
-        assert self.calc.subtraction(self, 2, 2) == 4
+        assert self.calc.subtraction(self, 2, 2) == 0
 
     def test_subtraction_calculation_failed(self):
-        assert self.calc.subtraction(self, 2, 2) == 4
+        assert self.calc.subtraction(self, 3, 2) == 0
 
     def test_adding_calculate_corrently(self):
         assert self.calc.adding(self, 2, 2) == 4
 
     def test_adding_calculation_failed(self):
-        assert self.calc.adding(self, 2, 2) == 4
+        assert self.calc.adding(self, 2, 3) == 4
 
