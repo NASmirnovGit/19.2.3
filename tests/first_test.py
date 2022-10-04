1985e948f987a5748b1f7ea5fa9ac540b0a64cb9
NASmirnovGit <nikus1727@gmail.com>
2022-10-04 21:09:28 +0700

    Initial commit

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,5 +1,5 @@
-import pytest
 from app.calculator import Calculator
+
 
 class TestCalc:
     def setup(self):
@@ -9,7 +9,7 @@ class TestCalc:
         assert self.calc.multiply(self, 2, 2) == 4
 
     def test_multiply_calculation_failed(self):
-        assert self.calc.multiply(self, 2, 2) ==5
+        assert self.calc.multiply(self, 2, 2) == 5
 
     def test_division_calculate_corrently(self):
         assert self.calc.division(self, 6, 3) == 2
@@ -28,4 +28,3 @@ class TestCalc:
 
     def test_adding_calculation_failed(self):
         assert self.calc.adding(self, 2, 3) == 4
-
